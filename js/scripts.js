4b832eea3e5d4459eb9a436a85bdcf8fbd4e607b
Halle Tempest and Ian Garner <halle+ian@>
2017-05-08 16:27:01 -0700

    Add HTML and UI/Business Logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,38 @@
 //Business Logic
-
-
-
-
-
-
+function placesMine(location, landmarks, season, activities, notes) {
+  this.locale = location,
+  this.landmarks = landmarks,
+  this.season = season,
+  this.activites = activities,
+  this.notes = notes
+}
 
 
 
 
 
 //UI Logic
-$document.ready(function() {
- $("form#new-place").submit(function(event)
-  event.preventDefault();
+$(document).ready(function() {
+  $("form#allForms").submit(function(event) {
+    event.preventDefault();
 
-}
-var location = $("input#location").val();
-var landmarks = $("input#landmarks").val();
-var season = $("input#season")val();
-var activities = $("input#activities").val();
-var notes = $("input#notes").val();
+    var inputtedLocation = $("input#location").val();
+    var inputtedLandmarks = $("input#landmarks").val();
+    var inputtedSeason = $("input#season").val();
+    var inputtedActivities = $("input#activities").val();
+    var inputtedNotes = $("input#notes").val();
+    var location = new placesMine(inputtedLocation, inputtedLandmarks, inputtedSeason, inputtedActivities, inputtedNotes);
 
-  $("ul#Places.append")("<li><span class='location'>" + locationTitle.location() + "</span></li>");
+    $("ul#Places").append("<li><span class='location'>" + location.locale + "</span></li>");
+  });
+
+  $("location").last().click(function(){
+    $("#show-location").show();
+    $("#show-location p").text(location, landmarks, season, activities, notes)
+    $(".location").text(inputtedLocation.locale);
+    $(".landmarks").text(inputtedLandmarks.landmarks);
+    $(".season").text(inputtedSeason.season);
+    $(".activities").text(inputtedActivities.activities);
+    $(".notes").text(inputtedNotes.notes);
+  });
+});
